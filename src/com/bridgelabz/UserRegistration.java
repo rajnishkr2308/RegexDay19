d5940b9bd036e07880fd105781161368c6e6f31c
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-06 23:47:06 +0530

    Added:Mobile Format

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Regex Problem");
         /*
         UC1: User need to enter Valid firstname
@@ -33,13 +34,26 @@ public class UserRegistration {
         Uc3:User need valid Email
          */
 
-        boolean result3 = Pattern.matches("[a-z]{3,}[.][a-z0-9]*@[a-z]{2,}[.][a-z]{2,}[.]*[a-z]*","rajneeshsingh9905@gmail.com");
+        boolean result3 = Pattern.matches("[a-z]{3,}[.][a-z0-9]*@[a-z]{2,}[.][a-z]{2,}[.]*[a-z]*", "rajneeshsingh9905@gmail.com");
         if (result3)
             System.out.println("Valid Email Address");
         else
             System.out.println("Invalid Email Address");
+        /*
+        U4:User need to follow pr-defined mobile Format
+         */
+
+        System.out.print("Enter Mobile number:- ");
+        String mobileNumber = sc.next();
+        Pattern pattern4 = Pattern.compile("^[0-9]{2,}[0-9]{10,}$");
+        Matcher matcher4 = pattern4.matcher("7541061533");
+        if (matcher4.matches()) {
+            System.out.println("valid");
+        } else {
+            System.out.println("Invalid");
 
         }
     }
+}
 
 
