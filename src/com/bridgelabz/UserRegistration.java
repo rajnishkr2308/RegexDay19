df7ca1255e84cab644ea7770d1bc69d856b89fc9
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-06 22:33:04 +0530

    Added:Enter Valid First Name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to Regex Problem");
+        /*
+        UC1: User need to enter Valid firstname
+         */
+//        Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}"); //For Reference
+//        Matcher matcher = pattern.matcher("Rajnish");  //For Reference
+
+        boolean result1 = Pattern.matches("^[A-Z][a-z]{2,}","Rajnish");
+        if (result1)
+            System.out.println("Valid First Name");
+        else
+            System.out.println("Invalid First Name");
     }
 }
