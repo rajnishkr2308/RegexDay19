60e428a24bef7bf5eb1d588d7bf3b41370daa21a
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-06 22:48:19 +0530

    Added:User need to valid Last Name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,4 +1,6 @@
 package com.bridgelabz;
+
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,10 +13,24 @@ public class UserRegistration {
 //        Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}"); //For Reference
 //        Matcher matcher = pattern.matcher("Rajnish");  //For Reference
 
-        boolean result1 = Pattern.matches("^[A-Z][a-z]{2,}","Rajnish");
+        boolean result1 = Pattern.matches("^[A-Z][a-z]{2,}", "Rajnish");
         if (result1)
             System.out.println("Valid First Name");
         else
             System.out.println("Invalid First Name");
+        /*
+        uc2: User need to enter Last name
+         */
+
+        Pattern pattern = Pattern.compile("^[A-Z]?[a-z]{3,}$");
+        Matcher matcher = pattern.matcher("Kumar");
+        Matcher matcher1 = pattern.matcher("Kumar");
+        if (matcher.matches()&& matcher1.matches())
+            System.out.println("valid");
+        else
+            System.out.println("Invalid");
+
+
+        }
     }
-}
+
